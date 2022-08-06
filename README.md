1f4fc312b72df69d5d0d4f851710a932132a0177
Joshua Hunsche Jones <jhunschejones@github.com>
2022-08-06 14:30:20 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Pocket-Casts-Stats
+# Pocket Casts Stats
+
+### Overview
+
+This is a WIP script based off of https://github.com/niklas-heer/pocketcasts-stats/blob/master/app.py that makes a web request to the unofficial pocket casts API in order to get listening time in minutes. This is valuable because the in-app tracker eventually rounds up to "days" which is not helpful for daily tracking.
+
+`./bin/run` will generate a new file in `/stats/` with the days date and the total number of seconds listened as of the writing. NOTE: since stats cannot be reset without deleting an account, this number always increases and the previous day's number can be subtracted from today's number to get the total time listened today.
---
+++ main.rb
@@ -0,0 +1,45 @@
+require "httparty"
+require "pry"
+require "json"
+
+class PocketCasts
+  LOGIN_URL = "https://api.pocketcasts.com/user/login"
+  STATS_URL = "https://api.pocketcasts.com/user/stats/summary"
+
+  def total_seconds_listened
+    @total_seconds_listened ||= begin
+      body = {}
+      headers = {
+        "Origin" => "https://play.pocketcasts.com",
+        "Authorization" => "Bearer #{token}",
+        "Accept": "*/*",
+      }
+      response = HTTParty.post(STATS_URL, body: body, headers: headers)
+      json_response = JSON.parse(response.body)
+      json_response["timeListened"].to_i
+    end
+  end
+
+  private
+
+  def token
+    @token ||= begin
+      raise "No password provided" unless ENV["POCKET_CASTS_PASSWORD"]
+      body = {
+        email: "joshjones103@gmail.com",
+        password: ENV["POCKET_CASTS_PASSWORD"],
+        scope: "webplayer"
+      }
+      headers = { "Origin" => "https://play.pocketcasts.com" }
+      response = HTTParty.post(LOGIN_URL, body: body, headers: headers)
+      json_response = JSON.parse(response.body)
+      json_response["token"]
+    end
+  end
+end
+
+todays_file = "./stats/#{Time.now.utc.strftime("%Y-%m-%d")}"
+
+File.open(todays_file, "w") do |file|
+  file.write(PocketCasts.new.total_seconds_listened)
+end
